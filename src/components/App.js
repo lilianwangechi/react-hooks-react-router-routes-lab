c044ff0a5e01615fa87242f4f2ea320093866be3
lilian <lilian.wangechi@student.moringaschool.com>
2022-10-04 21:40:48 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+//used switch instead of Routes to pass test
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
@@ -7,7 +8,48 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <NavBar />
+    <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/actors">
+          <Actors />
+        </Route>
+        <Route path="/directors">
+          <Directors />
+        </Route>
+    </Switch>
+  </div>;
 }
 
 export default App;
+
+
+// import React from "react";
+// import { Route, Routes } from "react-router-dom";
+// import NavBar from "./NavBar";
+// import Home from "./Home";
+// import Actors from "./Actors";
+// import Directors from "./Directors";
+// import Movies from "./Movies";
+
+// function App() {
+//   return <div>
+//     {/*{code here}*/}
+//     <NavBar/>
+// <Routes>
+//   <Route path="/" element={<Home />} />
+//   <Route path="/actors" element={<Actors/>} />
+//   <Route path="/directors" element={<Directors/>} />
+//   <Route path="/movies" element={<Movies/>} />
+//   </Routes>
+    
+//     </div>;
+// }
+
+// export default App;
